111a6b0d0801b422546e680bef7ec0ce7ab7a7d9
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2018-06-06 10:32:46 +0900

    Lineログインを実装

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,27 @@
+
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def line
+    basic_action
+  end
+
+  private
+
+  def basic_action
+    @omniauth = request.env['omniauth.auth']
+    if @omniauth.present?
+      @profile = SocialProfile.where(provider: @omniauth['provider'], uid: @omniauth['uid']).first
+      if @profile
+        @profile.set_values(@omniauth)
+        sign_in(:user, @profile.user)
+      else
+        @profile = SocialProfile.new(provider: @omniauth['provider'], uid: @omniauth['uid'])
+        email = @omniauth['info']['email'] ? @omniauth['info']['email'] : Faker::Internet.email
+        @profile.user = current_user || User.create!(email: email, name: @omniauth['info']['name'], password: Devise.friendly_token[0, 20])
+        @profile.set_values(@omniauth)
+        sign_in(:user, @profile.user)
+        redirect_to root_path and return
+      end
+    end
+    redirect_to root_path
+  end
+end
