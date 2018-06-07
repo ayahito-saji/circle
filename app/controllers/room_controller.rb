fe14ca0d2c5d4c142cbdb04c8d59e96c241868bc
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2018-06-07 22:04:56 +0900

    部屋作成の機能の実装

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -2,7 +2,21 @@ class RoomController < ApplicationController
   def index
   end
   def create
+    exit_room
+    room = Room.create()
+    current_user.update_attributes(room: room)
+    redirect_to root_path
   end
   def search
+    exit_room
+    redirect_to root_path
+  end
+
+  private
+  def exit_room
+    if current.room && current_user.room.user.count == 1
+      current_user.room.destroy
+    end
+    current_user.room = nil
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,8 @@ Rails.application.routes.draw do
       omniauth_callbacks: "omniauth_callbacks"
   }
   root 'pages#index'
-  resources :room do
-    collection do
-      get 'search'
-    end
-  end
+  post 'room', to: 'room#create'
+  post 'room/search', to: 'room#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
