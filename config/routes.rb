868a5aef4d421d56a219400320377e9fafb1c732
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2018-06-08 12:08:40 +0900

    routeのprefixを修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
       omniauth_callbacks: "omniauth_callbacks"
   }
   root 'pages#index'
-  post 'room/new', to: 'room#create'
-  post 'room/search', to: 'room#search'
-  delete 'room/exit', to: 'room#destroy'
+  post 'room/new', to: 'room#create', as: 'new_room'
+  post 'room/search', to: 'room#search', as: 'search_room'
+  delete 'room/exit', to: 'room#destroy', as: 'exit_room'
+  resources :rulebooks
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
