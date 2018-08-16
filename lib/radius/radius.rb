b2b2caf6cd2994ec575cb1c016dd83216494d132
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2018-08-16 19:29:01 +0900

    四則演算のタスクコードができた． フェイズ間のgoto文はうまいことやる．

--- lib/radius/radius.rb
+++ lib/radius/radius.rb
@@ -9,30 +9,134 @@ class Radius::Radius
     pp tree
 
     # コードを再帰的に評価してtask_codeを生成する
-    @task_code = evaluate(tree)
+    task_code = evaluate(tree)
 
-    pp @task_code
+    # pp task_code
 
-    return nil
+    return task_code
   end
 
   private
   def evaluate(tree)
 
-    operator = tree[0]  # 命令種類を規定
-    line = tree[1]      # 命令の行番号
-    value = tree[2]     # 値
-    child_trees = tree[3]  # 梢
+    operator = tree[0]      # 命令種類を規定
+    line = tree[1]          # 命令の行番号
+    value = tree[2]         # 値
+    child_trees = tree[3]   # 梢
 
     case operator
-      when :phases
+      when :phases # フェイズデータのハッシュから，リンクしてtask_codeを返す
         # フェイズごとに評価する(フェイズをまたぐgoto文が不完全)
         evaluated_phases = []
         child_trees.each do |child_tree|
           evaluated_phases << evaluate(child_tree)
         end
-      when :phase
+        pp evaluated_phases
+        return evaluated_phases[0][1]
 
+      when :phase # task_codeではなく，フェイズデータのハッシュを返す
+        phase_data = [
+          evaluate(child_trees[0]), # フェイズの識別子
+          evaluate(child_trees[1])  # フェイズのtask_code
+        ]
+        return phase_data
+
+      when :block
+        task_code = []
+        task_code << [:_block, line, value, nil]
+        child_trees.each do |child_tree|
+          task_code += evaluate(child_tree)
+        end
+        task_code << [:block, line, value, child_trees.length]
+        return task_code
+
+      # 四則演算
+      when :add
+        task_code = []
+        task_code += evaluate(child_trees[0])
+        task_code += evaluate(child_trees[1])
+        task_code << [:add, line, value, 2]
+        return task_code
+      when :dif
+        task_code = []
+        task_code += evaluate(child_trees[0])
+        task_code += evaluate(child_trees[1])
+        task_code << [:dif, line, value, 2]
+        return task_code
+      when :mul
+        task_code = []
+        task_code += evaluate(child_trees[0])
+        task_code += evaluate(child_trees[1])
+        task_code << [:mul, line, value, 2]
+        return task_code
+      when :div
+        task_code = []
+        task_code += evaluate(child_trees[0])
+        task_code += evaluate(child_trees[1])
+        task_code << [:div, line, value, 2]
+        return task_code
+      when :mod
+        task_code = []
+        task_code += evaluate(child_trees[0])
+        task_code += evaluate(child_trees[1])
+        task_code << [:mod, line, value, 2]
+        return task_code
+
+      # 等式・不等式
+      when :eq
+        task_code = []
+        task_code += evaluate(child_trees[0])
+        task_code += evaluate(child_trees[1])
+        task_code << [:eq, line, value, 2]
+        return task_code
+      when :neq
+        task_code = []
+        task_code += evaluate(child_trees[0])
+        task_code += evaluate(child_trees[1])
+        task_code << [:neq, line, value, 2]
+        return task_code
+      when :lt
+        task_code = []
+        task_code += evaluate(child_trees[0])
+        task_code += evaluate(child_trees[1])
+        task_code << [:lt, line, value, 2]
+        return task_code
+      when :lte
+        task_code = []
+        task_code += evaluate(child_trees[0])
+        task_code += evaluate(child_trees[1])
+        task_code << [:lte, line, value, 2]
+        return task_code
+      when :gt
+        task_code = []
+        task_code += evaluate(child_trees[0])
+        task_code += evaluate(child_trees[1])
+        task_code << [:gt, line, value, 2]
+        return task_code
+      when :gte
+        task_code = []
+        task_code += evaluate(child_trees[0])
+        task_code += evaluate(child_trees[1])
+        task_code << [:gte, line, value, 2]
+        return task_code
+
+      # リテラル
+      when :number
+        task_code = []
+        task_code << [:number, line, value, 0]
+        return task_code
+      when :string
+        task_code = []
+        task_code << [:string, line, value, 0]
+        return task_code
+      when :identifier
+        task_code = []
+        task_code << [:identifier, line, value, 0]
+        return task_code
+      when :boolean
+        task_code = []
+        task_code << [:boolean, line, value, 0]
+        return task_code
     end
   end
 end
--- lib/radius/radius_parser.rb
+++ lib/radius/radius_parser.rb
@@ -13,7 +13,7 @@ require 'pp'
 module Radius
   class RadiusParser < Racc::Parser
 
-module_eval(<<'...end radius_parser.ry/module_eval...', 'radius_parser.ry', 56)
+module_eval(<<'...end radius_parser.ry/module_eval...', 'radius_parser.ry', 55)
 
 def parse(str)
     scanner = StringScanner.new(str)
@@ -76,71 +76,70 @@ end
 ##### State transition tables begin ###
 
 racc_action_table = [
-    24,    25,    22,    23,    24,    25,    12,    10,    15,    17,
-    31,    20,     8,    17,     8,    20,     8,    17,     6,    20,
-     8,    17,     5,    20,     8,    17,     3,    20,     8,    24,
-    25,    22,    23,    17,    21,    20,     8,    24,    25 ]
+    23,    24,    21,    22,    23,    24,    12,    10,    16,    30,
+    19,     8,    16,     8,    19,     8,    16,     6,    19,     8,
+    16,     5,    19,     8,    16,     3,    19,     8,    23,    24,
+    21,    22,    16,    20,    19,     8,    23,    24 ]
 
 racc_action_check = [
-    26,    26,    26,    26,    28,    28,    11,     7,    11,    11,
-    26,    11,    11,    24,     5,    24,    24,    23,     3,    23,
-    23,    22,     2,    22,    22,    17,     1,    17,    17,    14,
-    14,    14,    14,    25,    13,    25,    25,    27,    27 ]
+    25,    25,    25,    25,    27,    27,    11,     7,    11,    25,
+    11,    11,    23,     5,    23,    23,    22,     3,    22,    22,
+    21,     2,    21,    21,    16,     1,    16,    16,    14,    14,
+    14,    14,    24,    13,    24,    24,    26,    26 ]
 
 racc_action_pointer = [
-   nil,    26,    16,    18,   nil,     0,   nil,     0,   nil,   nil,
-   nil,    -2,   nil,    25,    27,   nil,   nil,    14,   nil,   nil,
-   nil,   nil,    10,     6,     2,    22,    -2,    35,     2,   nil,
-   nil,   nil ]
+   nil,    25,    15,    17,   nil,     0,   nil,     0,   nil,   nil,
+   nil,    -2,   nil,    24,    26,   nil,    14,   nil,   nil,   nil,
+   nil,    10,     6,     2,    22,    -2,    34,     2,   nil,   nil,
+   nil ]
 
 racc_action_default = [
-    -2,   -21,    -1,   -21,    -3,   -21,    32,   -21,   -20,    -4,
-    -6,   -21,    -5,    -7,    -9,   -10,   -11,   -21,   -17,   -18,
-   -19,    -8,   -21,   -21,   -21,   -21,   -21,   -12,   -13,   -14,
-   -15,   -16 ]
+    -2,   -20,    -1,   -20,    -3,   -20,    31,   -20,   -19,    -4,
+    -6,   -20,    -5,    -7,    -9,   -10,   -20,   -16,   -17,   -18,
+    -8,   -20,   -20,   -20,   -20,   -20,   -11,   -12,   -13,   -14,
+   -15 ]
 
 racc_goto_table = [
-    14,     2,     4,     7,     1,    11,    26,    13,     9,   nil,
-   nil,    27,    28,    29,    30 ]
+    14,     2,     4,     7,     1,    25,    11,    13,     9,   nil,
+    26,    27,    28,    29 ]
 
 racc_goto_check = [
-     8,     2,     3,     4,     1,     6,     8,     7,     5,   nil,
-   nil,     8,     8,     8,     8 ]
+     8,     2,     3,     4,     1,     8,     6,     7,     5,   nil,
+     8,     8,     8,     8 ]
 
 racc_goto_pointer = [
-   nil,     4,     1,     0,    -2,     1,    -5,    -4,   -11,   nil,
+   nil,     4,     1,     0,    -2,     1,    -4,    -4,   -11,   nil,
    nil ]
 
 racc_goto_default = [
-   nil,   nil,   nil,   nil,    19,   nil,   nil,   nil,   nil,    16,
-    18 ]
+   nil,   nil,   nil,   nil,    18,   nil,   nil,   nil,   nil,    15,
+    17 ]
 
 racc_reduce_table = [
   0, 0, :racc_error,
-  1, 16, :_reduce_none,
-  0, 17, :_reduce_2,
-  2, 17, :_reduce_3,
-  3, 18, :_reduce_4,
-  3, 20, :_reduce_5,
-  0, 21, :_reduce_6,
-  2, 21, :_reduce_7,
-  3, 21, :_reduce_8,
+  1, 15, :_reduce_none,
+  0, 16, :_reduce_2,
+  2, 16, :_reduce_3,
+  3, 17, :_reduce_4,
+  3, 19, :_reduce_5,
+  0, 20, :_reduce_6,
+  2, 20, :_reduce_7,
+  3, 20, :_reduce_8,
+  1, 21, :_reduce_none,
   1, 22, :_reduce_none,
-  1, 22, :_reduce_none,
+  3, 22, :_reduce_11,
+  3, 22, :_reduce_12,
+  3, 22, :_reduce_13,
+  3, 22, :_reduce_14,
+  3, 22, :_reduce_15,
   1, 23, :_reduce_none,
-  3, 23, :_reduce_12,
-  3, 23, :_reduce_13,
-  3, 23, :_reduce_14,
-  3, 23, :_reduce_15,
-  3, 23, :_reduce_16,
-  1, 24, :_reduce_none,
+  1, 23, :_reduce_17,
   1, 24, :_reduce_18,
-  1, 25, :_reduce_19,
-  1, 19, :_reduce_20 ]
+  1, 18, :_reduce_19 ]
 
-racc_reduce_n = 21
+racc_reduce_n = 20
 
-racc_shift_n = 32
+racc_shift_n = 31
 
 racc_token_table = {
   false => 0,
@@ -153,13 +152,12 @@ racc_token_table = {
   "{" => 7,
   "}" => 8,
   ";" => 9,
-  :assign_statement => 10,
-  "(" => 11,
-  ")" => 12,
-  :NUMBER => 13,
-  :IDENTIFIER => 14 }
+  "(" => 10,
+  ")" => 11,
+  :NUMBER => 12,
+  :IDENTIFIER => 13 }
 
-racc_nt_base = 15
+racc_nt_base = 14
 
 racc_use_result_var = true
 
@@ -190,7 +188,6 @@ Racc_token_to_s_table = [
   "\"{\"",
   "\"}\"",
   "\";\"",
-  "assign_statement",
   "\"(\"",
   "\")\"",
   "NUMBER",
@@ -268,61 +265,59 @@ module_eval(<<'.,.,', 'radius_parser.ry', 22)
 
 # reduce 10 omitted
 
-# reduce 11 omitted
+module_eval(<<'.,.,', 'radius_parser.ry', 30)
+  def _reduce_11(val, _values, result)
+    result = [:add, val[1][:line], val[1][:value], [val[0], val[2]]]
+    result
+  end
+.,.,
 
 module_eval(<<'.,.,', 'radius_parser.ry', 31)
   def _reduce_12(val, _values, result)
-    result = [:add, val[1][:line], val[1][:value], [val[0], val[2]]]
+    result = [:dif, val[1][:line], val[1][:value], [val[0], val[2]]]
     result
   end
 .,.,
 
 module_eval(<<'.,.,', 'radius_parser.ry', 32)
   def _reduce_13(val, _values, result)
-    result = [:dif, val[1][:line], val[1][:value], [val[0], val[2]]]
+    result = [:mul, val[1][:line], val[1][:value], [val[0], val[2]]]
     result
   end
 .,.,
 
 module_eval(<<'.,.,', 'radius_parser.ry', 33)
   def _reduce_14(val, _values, result)
-    result = [:mul, val[1][:line], val[1][:value], [val[0], val[2]]]
+    result = [:div, val[1][:line], val[1][:value], [val[0], val[2]]]
     result
   end
 .,.,
 
 module_eval(<<'.,.,', 'radius_parser.ry', 34)
   def _reduce_15(val, _values, result)
-    result = [:dev, val[1][:line], val[1][:value], [val[0], val[2]]]
-    result
-  end
-.,.,
-
-module_eval(<<'.,.,', 'radius_parser.ry', 35)
-  def _reduce_16(val, _values, result)
     result = val[1]
     result
   end
 .,.,
 
-# reduce 17 omitted
+# reduce 16 omitted
 
-module_eval(<<'.,.,', 'radius_parser.ry', 39)
-  def _reduce_18(val, _values, result)
+module_eval(<<'.,.,', 'radius_parser.ry', 38)
+  def _reduce_17(val, _values, result)
     result = [:reference_variable, nil, nil, [val[0]]]
     result
   end
 .,.,
 
-module_eval(<<'.,.,', 'radius_parser.ry', 42)
-  def _reduce_19(val, _values, result)
+module_eval(<<'.,.,', 'radius_parser.ry', 41)
+  def _reduce_18(val, _values, result)
     result = [:number, val[0][:line], val[0][:value], []]
     result
   end
 .,.,
 
-module_eval(<<'.,.,', 'radius_parser.ry', 45)
-  def _reduce_20(val, _values, result)
+module_eval(<<'.,.,', 'radius_parser.ry', 44)
+  def _reduce_19(val, _values, result)
     result = [:identifier, val[0][:line], val[0][:value], []]
     result
   end
